f02664a9fc783de68d4c82c92a16c9a6de0690c1
John Rasine Irem <rajonirem8@gmail.com>
2023-09-08 18:37:44 +0100

    all attributes shown

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const day = document.querySelector('.day')
 const time = document.querySelector('.time')
 const date = new Date()
-day.append(date.getDay())
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+day.append(days[date.getDay()])
 time.append(date.getUTCMilliseconds())
